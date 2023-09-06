da9c8bf086f51de7d93debd1290d10cfb1158b74
gwonsheep <jae-kwen@daum.net>
2023-09-06 17:24:56 +0900

    9/6 gwon (17:24)

---
+++ src/main/java/com/project/jeans/controller/map/MapController.java
@@ -0,0 +1,34 @@
+package com.project.jeans.controller.map;
+
+import com.project.jeans.LoginCheckSession;
+import com.project.jeans.domain.member.dto.MemberDTO;
+import com.project.jeans.service.member.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class MapController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/map")
+    public String getMap(HttpSession session, Model model){
+
+        LoginCheckSession loginCheck = new LoginCheckSession(memberService);
+        MemberDTO memberInfo = loginCheck.getLoginCheckSession(session, model);
+
+        // 로그인이 필요한 경우 리디렉션
+        if (memberInfo == null) {return "/member/login";}
+
+        model.addAttribute("member_name",memberInfo.getMember_name());
+        model.addAttribute("member_class",memberInfo.getMember_class());
+        model.addAttribute("member_type",memberInfo.getMember_type());
+
+        return "/map/map";
+    }
+
+}
