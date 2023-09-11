2e471fc4d276f9a7ce5a63b2083990d4b7ddedbd
gwonsheep <jae-kwen@daum.net>
2023-09-11 12:56:33 +0900

    9/11 gwon (12:55) board1에 파일 관련 기능 추가 <코드 배워갑니다>

--- src/main/webapp/resources/js/board/boardWrite.js
+++ src/main/webapp/resources/js/board/boardWrite.js
@@ -1,3 +1,4 @@
+/* 글 작성 여부 확인 */
 $(document).ready(function() {
     $("#save").click(function() {
         var confirmed = confirm("저장하시겠습니까?");
@@ -8,4 +9,54 @@ $(document).ready(function() {
             return false;
         }
     });
+});
+
+/* 글 작성 (사진 첨부) */
+document.addEventListener("DOMContentLoaded", function() {
+  const saveButton = document.querySelector('#save');
+  if (saveButton) {
+    saveButton.addEventListener('click', function() {
+      var board1_content = document.querySelector('#board1_content').innerHTML;
+      var hiddenInput = document.createElement('input');
+      var form = document.querySelector('form');
+      hiddenInput.type = 'hidden';
+      hiddenInput.name = 'board1_content';
+      hiddenInput.value = board1_content;
+      form.appendChild(hiddenInput);
+    });
+  } else {
+    console.error("업로드 실패");
+  }
+
+  $('.content-file').click(function() {
+    $('#photo-input').trigger('click');
+  });
+
+  const photoInput = document.getElementById('photo-input');
+  const previewContainer = document.querySelector('#board1_content');
+
+  if (photoInput && previewContainer) {
+    photoInput.addEventListener('change', function(event) {
+      const files = event.target.files;
+
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        if (file.type.startsWith('image/')) {
+          const reader = new FileReader();
+          reader.onload = function(e) {
+            const imageSrc = e.target.result;
+            createPreviewImage(imageSrc);
+          };
+          reader.readAsDataURL(file);
+        }
+      }
+    });
+  }
+
+  function createPreviewImage(imageSrc) {
+    const imageElement = document.createElement('img');
+    imageElement.src = imageSrc;
+    imageElement.classList.add('preview-image');
+    previewContainer.appendChild(imageElement);
+  }
 });
